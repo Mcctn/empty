671dcd1b6d8ca8fa6bcabc6aa348ae358d9b09f9
Martin <martim.cornen@efrei.net>
2022-03-10 10:40:00 -0500

    emd session 1

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,17 @@
 <script>
 import Navbar from './components/Navbar.vue'
 import Hero from './components/Hero.vue'
+import Card from "./components/card.vue";
 export default {
   data(){
     return{
+      title:"",
+
       message: "This is an arg"
     }
   },
   components:{
+    Card,
     Navbar,
     Hero,
   }
@@ -19,6 +23,23 @@ export default {
   <h1>{{message}}</h1>
   <Navbar></Navbar>
   <Hero> </Hero>
+
+
+  <form>
+    <h2>Please enter class title</h2>
+    <input v-model="title" type="text">
+
+    <h2>Value: {{title}}</h2>
+
+    <button @click="handleClick">Create</button>
+    <!--
+    <template v-for="a in cards">
+      <card v-bind:description="a.detail", v-bind:title="a.title"></card>
+    </template>-->
+
+
+
+  </form>
 </template>
 
 <style>
